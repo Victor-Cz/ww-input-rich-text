0e28a3e9a91bfd8669a28104ded937aea4a37716
Victor <victorcaz56@outlook.fr>
2025-06-18 16:14:06 +0200

    new items

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -330,10 +330,45 @@ export default {
                 icon: `    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
          xmlns="http://www.w3.org/2000/svg" stroke="#000" stroke-width="2"
          stroke-linecap="round" stroke-linejoin="round">
-      <path d="M6 4h8a4 4 0 0 1 0 8H6zm0 8h9a4 4 0 0 1 0 8H6z"/>
+      <path d="M10 3H20M4 21H14M15 3L9 21"/>
     </svg>`,
             },
-            // Ajoute toutes tes actions ici
+            {
+                name: "underline",
+                title: "Underline",
+                icon: `    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+         xmlns="http://www.w3.org/2000/svg" stroke="#000" stroke-width="2"
+         stroke-linecap="round" stroke-linejoin="round">
+      <path d="M4 21H20M18 4V11C18 14.3137 15.3137 17 12 17C8.68629 17 6 14.3137 6 11V4M4 3H8M16 3H20" />
+    </svg>`,
+            },
+            {
+                name: "strike",
+                title: "Strike",
+                icon: `    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+         xmlns="http://www.w3.org/2000/svg" stroke="#000" stroke-width="2"
+         stroke-linecap="round" stroke-linejoin="round">
+      <path d="M17 5h-7a3 3 0 0 0-3 3v1a3 3 0 0 0 3 3h7M7 19h7a3 3 0 0 0 3-3v-1M5 12h14" />
+    </svg>`,
+            },
+            {
+                name: "code",
+                title: "Code",
+                icon: `    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+         xmlns="http://www.w3.org/2000/svg" stroke="#000" stroke-width="2"
+         stroke-linecap="round" stroke-linejoin="round">
+      <path d="M7 8L3 11.6923L7 16M17 8L21 11.6923L17 16M14 4L10 20" />
+    </svg>`,
+            },
+            {
+                name: "bulletList",
+                title: "Bullet List",
+                icon: `    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+         xmlns="http://www.w3.org/2000/svg" stroke="#000" stroke-width="2"
+         stroke-linecap="round" stroke-linejoin="round">
+      <path d="M8 6.00067L21 6.00139M8 12.0007L21 12.0015M8 18.0007L21 18.0015M3.5 6H3.51M3.5 12H3.51M3.5 18H3.51M4 6C4 6.27614 3.77614 6.5 3.5 6.5C3.22386 6.5 3 6.27614 3 6C3 5.72386 3.22386 5.5 3.5 5.5C3.77614 5.5 4 5.72386 4 6ZM4 12C4 12.2761 3.77614 12.5 3.5 12.5C3.22386 12.5 3 12.2761 3 12C3 11.7239 3.22386 11.5 3.5 11.5C3.77614 11.5 4 11.7239 4 12ZM4 18C4 18.2761 3.77614 18.5 3.5 18.5C3.22386 18.5 3 18.2761 3 18C3 17.7239 3.22386 17.5 3.5 17.5C3.77614 17.5 4 17.7239 4 18Z" />
+    </svg>`,
+            },
         ],
     }),
     watch: {
@@ -925,13 +960,13 @@ export default {
         height: 28px;
     }
 
+    .is-active {
+        background-color: #333;
+    }
+
     .btn:hover {
         background-color: #f0f0f0;
         border: "1px solid #2483E2";
-    }
-
-    .is-active {
-        background-color: #333;
     }
 
     .is-active svg {
