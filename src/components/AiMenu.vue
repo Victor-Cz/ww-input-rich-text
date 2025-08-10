1624f5e3341eb6adc5c3e2f5e575afa60416526c
Victor <victorcaz56@outlook.fr>
2025-08-10 12:55:50 +0200

    styles

--- src/components/AiMenu.vue
+++ src/components/AiMenu.vue
@@ -513,7 +513,7 @@ export default {
     overflow: hidden;
     z-index: 1000;
     padding: 4px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 1px 8px var(--primary-color-1A);
     min-width: 180px;
     min-height: 100px;
     background: white;
