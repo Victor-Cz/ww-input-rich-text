b3d77355bfb6347bfab23c7a45c1ab4c715bdb60
Victor <victorcaz56@outlook.fr>
2025-08-10 14:35:07 +0200

    logs

--- src/extensions/SelectionHighlighter.js
+++ src/extensions/SelectionHighlighter.js
@@ -1,44 +1,14 @@
 import { Extension } from '@tiptap/core'
-import { Decoration, DecorationSet } from 'prosemirror-view'
-import { Plugin } from 'prosemirror-state'
 
 export const SelectionHighlighter = Extension.create({
   name: 'selectionHighlighter',
 
+  onCreate() {
+    console.log('SelectionHighlighter - Extension créée');
+  },
+
   addProseMirrorPlugins() {
-    return [
-      new Plugin({
-        state: {
-          init() {
-            console.log('SelectionHighlighter - Plugin initialisé');
-            return DecorationSet.empty
-          },
-          apply(tr, old) {
-            // Récupérer le storedSelection depuis les métadonnées de la transaction
-            const storedSelection = tr.getMeta('storedSelection')
-            
-            console.log('SelectionHighlighter - storedSelection:', storedSelection);
-            
-            if (!storedSelection) {
-              return DecorationSet.empty
-            }
-
-            const deco = Decoration.inline(
-              storedSelection.from,
-              storedSelection.to,
-              { style: 'background-color: yellow;' }
-            )
-
-            console.log('SelectionHighlighter - decoration créée:', deco);
-            return DecorationSet.create(tr.doc, [deco])
-          },
-        },
-        props: {
-          decorations(state) {
-            return this.getState(state)
-          },
-        },
-      })
-    ]
+    console.log('SelectionHighlighter - addProseMirrorPlugins appelé');
+    return []
   },
 })
