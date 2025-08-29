fa1713bf415a2f25c9f2d2988d659f0f2ecffad0
Victor <victorcaz56@outlook.fr>
2025-08-29 16:18:10 +0200

    test

--- src/extensions/SafeLinks.js
+++ src/extensions/SafeLinks.js
@@ -33,8 +33,13 @@ export const SafeLinks = Extension.create({
                 console.log('SafeLinks: Clic sur un lien détecté')
                 // Bloquer si l'utilisateur ne maintient PAS Cmd (Mac) ou Ctrl (Win)
                 if (!(event.metaKey || event.ctrlKey)) {
+                  console.log('SafeLinks: Blocage du lien (pas de Cmd/Ctrl)')
                   event.preventDefault()
+                  event.stopPropagation()
+                  event.stopImmediatePropagation()
                   return true // Empêche le comportement par défaut (ouvrir le lien)
+                } else {
+                  console.log('SafeLinks: Lien autorisé (Cmd/Ctrl enfoncé)')
                 }
               }
 
